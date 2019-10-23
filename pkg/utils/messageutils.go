cf9b9ffb8903a9799e5195f6fd553b10455e360a
C.V.Hariharan <cvhariharan@protonmail.com>
2019-10-23 19:10:16 +0530

    Added utility functions for redis and messaging

---
+++ pkg/utils/messageutils.go
@@ -0,0 +1,11 @@
+package utils
+
+type TaskAction struct {
+	ID string
+	Action string
+}
+
+type UploadInfo struct {
+	Filename string
+	Username string
+}
---
+++ pkg/utils/redisutils.go
@@ -0,0 +1,65 @@
+package utils
+
+import (
+	"github.com/gomodule/redigo/redis"
+)
+
+type TaskInfo struct {
+	ID string
+	Owner string
+	Status string
+	TaskServer string
+}
+
+// Insert the task into a hashmap with key as taskserver:id and also add 
+// taskserver:id to a set corresponding to each username
+func InsertTask(conn redis.Conn, id, owner, status, taskserver string) bool {
+	t := TaskInfo{id, owner, status, taskserver}
+	taskId := taskserver+":"+id
+	_, err := conn.Do("HMSET", redis.Args{taskId}.AddFlat(t)...)
+	if err != nil {
+		return false
+	}
+	_, err = conn.Do("SADD", owner, taskId)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// Checks if the task owner is the user and if yes, returns the TaskInfo
+// else returns nil
+func GetTask(conn redis.Conn, id, taskserver, username string) TaskInfo {
+	var t TaskInfo
+	taskId := taskserver+":"+id
+	exists, err := redis.Bool(conn.Do("SISMEMBER", username, taskId))
+	if err != nil {
+		panic(err)
+	}
+	if exists {
+		value, err := redis.Values(conn.Do("HGETALL", taskId))
+		if err != nil {
+			panic(err)
+		}
+		err = redis.ScanStruct(value, &t)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return t
+}
+
+// Returns an array of taskserver:id for a given user
+func GetAllTasks(conn redis.Conn, username string) []string {
+	var results []string
+	var err error
+	results, err = redis.Strings(conn.Do("SMEMBERS", username))
+	if err != nil {
+		panic(err)
+	}
+	return results
+}
+
+
+
+
