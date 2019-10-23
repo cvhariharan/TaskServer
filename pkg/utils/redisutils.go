ab32203e98a0993ab5c84dcdee654083d2a4bc82
C.V.Hariharan <cvhariharan@protonmail.com>
2019-10-23 22:19:41 +0530

    Added status update and owner checking functions

--- pkg/utils/redisutils.go
+++ pkg/utils/redisutils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"log"
 	"github.com/gomodule/redigo/redis"
 )
 
@@ -21,6 +22,10 @@ func InsertTask(conn redis.Conn, id, owner, status, taskserver string) bool {
 		return false
 	}
 	_, err = conn.Do("SADD", owner, taskId)
+	if err != nil {
+		return false
+	}
+	_, err = conn.Do("SET", id, taskserver)
 	if err != nil {
 		return false
 	}
@@ -49,15 +54,40 @@ func GetTask(conn redis.Conn, id, taskserver, username string) TaskInfo {
 	return t
 }
 
+func UpdateStatus(conn redis.Conn, taskId, status string) bool {
+	_, err := conn.Do("HSET", taskId, "Status", status)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
+
+// Returns true if the user is the task owner else false
+func IsOwner(conn redis.Conn, taskId, username string) bool {
+	exists, err := redis.Bool(conn.Do("SISMEMBER", username, taskId))
+	if err != nil {
+		panic(err)
+	}
+	return exists
+}
+
 // Returns an array of taskserver:id for a given user
 func GetAllTasks(conn redis.Conn, username string) []string {
-	var results []string
-	var err error
-	results, err = redis.Strings(conn.Do("SMEMBERS", username))
+	results, err := redis.Strings(conn.Do("SMEMBERS", username))
 	if err != nil {
 		panic(err)
 	}
 	return results
+}
+
+// Returns the taskserver for a given task id
+func GetServer(conn redis.Conn, pid string) string {
+	result, err := redis.String(conn.Do("GET", pid))
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
 
 
